3ad4db7a44584e3e0cfb7db3d1425a6c521cae46
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-11-29 21:18:21 +0200

    Tts added and working

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,28 @@
 import { useState, useEffect } from "react";
 
+let readSpeech = false;
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+
 const Homepage = () => {
     const [amountOfSnippets, setamountOfSnippets] = useState('')
     const [text, setText] = useState('')
     const [failed, setFailed] = useState('')
+    const [soundText, setSoundText] = useState('')
+    const [button, setButton] = useState('')
     const url = "/api/generateText";
+
+    const textReader = async () => {
+        if (readSpeech) {
+            readSpeech = false;
+            setButton("On");
+            setSoundText("speech Off")
+        } else {
+            readSpeech = true;
+            setButton("Off");
+            setSoundText("speech On");
+        }
+    }
 
     const handleSubmit = async () => {
 
@@ -27,6 +45,8 @@ const Homepage = () => {
                 if (data.success) {
                     setText(data.message)
                     setFailed("")
+                    speech.text = data.message;
+                    if (readSpeech) window.speechSynthesis.speak(speech)
                 }
                 if (data.failure) {
                     setFailed(data.error)
@@ -42,7 +62,8 @@ const Homepage = () => {
     }
 
     useEffect(() => {
-        console.log('Entry was made')
+        setButton("Off");
+        setSoundText("speech On")
     }, [])
 
     return (
@@ -55,6 +76,14 @@ const Homepage = () => {
                 value={failed}
             ></textarea>
             <h2 className="container-title">TextSnippeting</h2>
+            <button
+                className="sound-button"
+                onClick={textReader}
+            >{button}</button>
+            <small
+                className="sound-text"
+                onChange={e => setSoundText(e.target.value)}
+            >{soundText}</small>
             <input
                 type="text"
                 required
